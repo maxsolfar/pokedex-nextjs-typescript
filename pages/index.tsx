d49bfe246c89e776ffc6be1ee7668c021fe93d18
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-09-27 00:49:50 -0500

    Modified: Home with StaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,41 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { GetStaticProps, NextPage } from "next";
+import { MenuNavBar } from "../components/ui";
+import { PokeAPI } from "../api";
+import { PokemonListResponse, SinglePokemon } from "../interfaces";
+import { Container, Grid } from "@nextui-org/react";
+import { PokemonCard } from "../components/pokemon";
 
-const HomePage: NextPage = () => {
-  return (
-    <>
-      <h1>Home</h1>
-    </>
-  )
+interface IProps {
+  pokemons: SinglePokemon[];
 }
 
-export default HomePage
+const HomePage: NextPage<IProps> = ({ pokemons }) => {
+  return (
+    <>
+      <MenuNavBar />
+      <Container lg>
+        <Grid.Container gap={2} justify={"flex-start"}>
+          {pokemons?.map((pokemon) => (
+            <PokemonCard key={pokemon.id} pokemon={pokemon}/>
+          ))}
+        </Grid.Container>
+      </Container>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await PokeAPI.get<PokemonListResponse>("/pokemon?limit=151");
+  const pokemons: SinglePokemon[] = data.results?.map((pokemon, index) => ({
+    ...pokemon,
+    id: index + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${index + 1}.png`,
+  }));
+  return {
+    props: {
+      pokemons,
+    },
+  };
+};
+
+export default HomePage;
